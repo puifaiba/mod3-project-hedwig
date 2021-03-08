4be1983dfe9f727dd6d7e533d29485e83960ca3b
puifaiba <puifai.ba@gmail.com>
2021-03-08 13:37:04 -0500

    assocs

--- mod3-project-backend/app/models/user.rb
+++ mod3-project-backend/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :chatroom_users
+    has_many :chatrooms, through: :chatroom_users
+    has_many :messages
+    has_many :house_users
+    has_many :houses, through: :house_users
 end
