5f8d276226d043d941ac9021c9cb41fc1e4c0b1f
Abraham Aviles <aaaviles1995@gmail.com>
2021-03-10 01:55:20 -0500

    built sidebar resizer

--- mod3-project-frontend/src/index.js
+++ mod3-project-frontend/src/index.js
@@ -12,13 +12,30 @@ const barResizer = () => {
     const resize_bars = document.querySelectorAll('.resizer')
     for(let i=0; i< resize_bars.length; i++){
         const current_resize_bar = resize_bars[i]
+        const current_bar = current_resize_bar.parentNode
         current_resize_bar.addEventListener('mousedown', (event) => {
             event.preventDefault()
-            const current_bar = current_resize_bar.parentNode
             original_width = parseFloat(getComputedStyle(current_bar, null).getPropertyValue('width').replace('px', ''));
             original_mouse_x = event.pageX;
             window.addEventListener('mousemove',resize)
             window.addEventListener('mouseup', stopResize)
+        })
+        current_resize_bar.addEventListener('dblclick', (event) => {
+            event.preventDefault()
+            const workspace_container = current_bar.parentNode
+            const left_bar = document.querySelector('.workspace_left_bar')
+            const left_width = parseFloat(getComputedStyle(left_bar, null).getPropertyValue('width').replace('px', ''));
+            const right_bar = document.querySelector('.workspace_right_bar')
+            const right_width = parseFloat(getComputedStyle(right_bar, null).getPropertyValue('width').replace('px', ''));
+            if(current_bar === document.querySelector('.workspace_left_bar')){
+                current_bar.style.width = '250px'
+                current_resize_bar.style.left = '245px'
+                workspace_container.style.gridTemplateColumns = `250px auto ${right_width}px`
+            }else if(current_bar === document.querySelector('.workspace_right_bar')){
+                current_bar.style.width = '350px'
+                current_resize_bar.style.right = '345px'
+                workspace_container.style.gridTemplateColumns = `${left_width}px auto 350px`
+            }
         })
         const resize = (event) => {
             const current_bar = current_resize_bar.parentNode
