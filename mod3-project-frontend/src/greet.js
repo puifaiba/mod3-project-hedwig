2edfeef04f9c92c54c0d02909ec1040d92aa8a9f
puifaiba <puifai.ba@gmail.com>
2021-03-11 17:33:18 -0500

    login page

--- mod3-project-frontend/src/greet.js
+++ mod3-project-frontend/src/greet.js
@@ -2,19 +2,48 @@ document.addEventListener("DOMContentLoaded", () => {
   loginPage()
 })
 
+USERS_URL = "http://localhost:3000/users/"
+SORTINGHAT_URL = "https://www.potterapi.com/v1/sortinghat"
+
 const clientContainer = document.querySelector(".client_container")
+const loginSection = document.querySelector(".login-section")
 const loginForm = document.querySelector(".login-form")
+const sortingHatSection = document.querySelector(".sorting_hat_section")
 const profileUser = document.querySelector(".profile_username")
 
 function loginPage() {
   clientContainer.style.display = "none"
+  sortingHatSection.style.display = "none"
   loginForm.addEventListener("submit", (event) => handleLoginSubmit(event))
 }
 
 function handleLoginSubmit(event) {
   event.preventDefault()
-  const username = loginForm.username.value
-  console.log(username)
-  profileUser.innerText = username
-  barResizer()
+  const sessionUser = loginForm.username.value
+  let found = false
+  fetch(USERS_URL)
+    .then((res) => res.json())
+    .then((usersData) => {
+      usersData.forEach((user) => {
+        if (user.username === sessionUser) {
+          found = true
+          barResizer()
+        }
+      })
+    })
+
+  if (!found) {
+    sortingHat(sessionUser)
+  }
+
+  loginForm.reset()
+}
+
+function sortingHat(sessionUser) {
+  fetch(SORTINGHAT_URL)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+
+  loginSection.remove()
+  sortingHatSection.style.display = "block"
 }
