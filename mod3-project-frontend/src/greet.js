dbafbc09961421a0677142db12ab1455e8137344
Abraham Aviles <aaaviles1995@gmail.com>
2021-03-11 21:56:16 -0500

    merged

--- mod3-project-frontend/src/greet.js
+++ mod3-project-frontend/src/greet.js
@@ -1,49 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
-  loginPage()
-})
+// document.addEventListener("DOMContentLoaded", () => {
+//   loginPage()
+// })
 
-USERS_URL = "http://localhost:3000/users/"
-SORTINGHAT_URL = "https://www.potterapi.com/v1/sortinghat"
+// USERS_URL = "http://localhost:3000/users/"
+// SORTINGHAT_URL = "https://www.potterapi.com/v1/sortinghat"
 
-const clientContainer = document.querySelector(".client_container")
-const loginSection = document.querySelector(".login-section")
-const loginForm = document.querySelector(".login-form")
-const sortingHatSection = document.querySelector(".sorting_hat_section")
-const profileUser = document.querySelector(".profile_username")
+// const clientContainer = document.querySelector(".client_container")
+// const loginSection = document.querySelector(".login-section")
+// const loginForm = document.querySelector(".login-form")
+// const sortingHatSection = document.querySelector(".sorting_hat_section")
+// const profileUser = document.querySelector(".profile_username")
 
-function loginPage() {
-  clientContainer.style.display = "none"
-  sortingHatSection.style.display = "none"
-  loginForm.addEventListener("submit", (event) => handleLoginSubmit(event))
-}
+// function loginPage() {
+//   loginForm.addEventListener("submit", (event) => handleLoginSubmit(event))
+// }
 
-function handleLoginSubmit(event) {
-  event.preventDefault()
-  const sessionUser = loginForm.username.value
-  let found = false
-  fetch(USERS_URL)
-    .then((res) => res.json())
-    .then((usersData) => {
-      usersData.forEach((user) => {
-        if (user.username === sessionUser) {
-          found = true
-          barResizer()
-        }
-      })
-    })
+// function handleLoginSubmit(event) {
+//   event.preventDefault()
+//   const sessionUser = loginForm.username.value
+//   fetch(USERS_URL)
+//     .then((res) => res.json())
+//     .then((usersData) => {
+//       let found = false
+//       usersData.forEach((user) => {
+//         if (user.username === sessionUser) {
+//           found = true
+//           sortingHatSection.remove()
+//           return runHPChat()
+//         }
+//       })
+//       if (!found) {
+//         return sortingHat(sessionUser)
+//       }
+//     })
+//   loginForm.reset()
+// }
 
-  if (!found) {
-    sortingHat(sessionUser)
-  }
+// function sortingHat(sessionUser) {
+//   fetch(SORTINGHAT_URL)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data)
+//       const outcome = document.createElement("h2")
+//       outcome.innerText = data
+//       document.querySelector(".sorting_hat_outcome").append(outcome)
+//     })
 
-  loginForm.reset()
-}
-
-function sortingHat(sessionUser) {
-  fetch(SORTINGHAT_URL)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-
-  loginSection.remove()
-  sortingHatSection.style.display = "block"
-}
+//   loginSection.remove()
+//   sortingHatSection.style.display = "block"
+// }
--- mod3-project-frontend/src/index.js
+++ mod3-project-frontend/src/index.js
@@ -1,4 +1,60 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Boonie Code~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+document.addEventListener("DOMContentLoaded", () => {
+    loginPage()
+})
+  
+USERS_URL = "http://localhost:3000/users/"
+SORTINGHAT_URL = "https://www.potterapi.com/v1/sortinghat"
+  
+const clientContainer = document.querySelector(".client_container")
+const loginSection = document.querySelector(".login-section")
+const loginForm = document.querySelector(".login-form")
+const sortingHatSection = document.querySelector(".sorting_hat_section")
+const profileUser = document.querySelector(".profile_username")
+  
+function loginPage() {
+    loginForm.addEventListener("submit", (event) => handleLoginSubmit(event))
+}
+  
+function handleLoginSubmit(event) {
+    event.preventDefault()
+    const sessionUser = loginForm.username.value
+    fetch(USERS_URL)
+      .then((res) => res.json())
+      .then((usersData) => {
+        let found = false
+        usersData.forEach((user) => {
+          if (user.username === sessionUser) {
+            found = true
+            sortingHatSection.remove()
+            return runHPChat()
+          }
+        })
+        if (!found) {
+          return sortingHat(sessionUser)
+        }
+      })
+    loginForm.reset()
+}
+  
+function sortingHat(sessionUser) {
+    fetch(SORTINGHAT_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        const outcome = document.createElement("h2")
+        outcome.innerText = data
+        document.querySelector(".sorting_hat_outcome").append(outcome)
+      })
+  
+        loginSection.remove()
+        sortingHatSection.style.display = "block"
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Abraham Code~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const runHPChat = () =>{
     barResizer()
     buttonEvents()
 
@@ -17,8 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
             editor.on('keydown', submitMain)
         }
     });
-
-})
+}
 
 const buttonEvents = () => {
     document.querySelector('.right_close').addEventListener('click', closeRight)
@@ -29,6 +84,7 @@ const buttonEvents = () => {
 
 const barResizer = () => {
     clientContainer.style.display = "block"
+    sortingHatSection.style.display = "none"
     const left_min_size = 180
     const left_max_size = 600
     const right_min_size = 260
